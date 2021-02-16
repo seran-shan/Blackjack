8c930f809a64cdba1e7829abaff94cbd81f5ce63
Seran Shanmugathas <Seran@live.no>
2021-02-16 22:29:50 +0100

    Oppdatert CardDeck

--- project/src/main/java/blackjack/CardDeck.java
+++ project/src/main/java/blackjack/CardDeck.java
@@ -1,5 +1,51 @@
 package blackjack;
 
-public class CardDeck {
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class CardDeck {
+	private ArrayList<Card> cards = new ArrayList<>();
+	//private final int NUM_OF_CARDS = 52; //Muligens bruke denne for å kontrollere at kortstokken inneholder 52 kort.
+	private final int CARDS_PER_TYPE = 13;
+	
+	
+	/**
+	 * Opprettes en hel kortstokk når et instans av klassen CardDeck opprettes.
+	 */
+	public CardDeck() {
+		createFullDeck();
+		
+		
+	}
+	
+	/**
+	 * Get-metode
+	 * @return liste av alle kortene i bunken
+	 */
+	public ArrayList<Card> getCards() {
+        return cards;
+    }
+	
+	/**
+	 * Bruker shuffle metoden i Collections biblioteket.
+	 */
+	public void shuffleDeck() {
+		Collections.shuffle(cards);
+	}
+	
+	/**
+	 * Burde vurdere å lage "enums" for suit og face, slik at vi slipper å definere suits og face i denne metoden.
+	 */
+	public void createFullDeck() {
+		//String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
+		char[] suits = {'S', 'H', 'D', 'C'};
+		
+		for (char suit : suits) {
+			for (int i = 1; i <= CARDS_PER_TYPE; i++) {
+				cards.add(new Card(suit, i));
+			}
+ 		}
+	}
+	
+	
 }
